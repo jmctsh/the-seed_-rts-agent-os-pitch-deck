67ffb892807090991f0b25f9e3b314f53dd6451f
jmctsh <690019012@qq.com>
2025-12-20 13:00:47 +0800

    feat: 增加单文件构建模式

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, '.', '');
+    const isSingleFile = process.env.BUILD_SINGLE_FILE === 'true';
+
     return {
       base: command === 'serve' ? '/' : './',
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        isSingleFile && viteSingleFile()
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
@@ -19,6 +25,9 @@ export default defineConfig(({ command, mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        assetsInlineLimit: isSingleFile ? 100000000 : 4096, // Force inline assets for single file build
       }
     };
 });
